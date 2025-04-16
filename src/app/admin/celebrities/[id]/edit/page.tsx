06f826891ce8d65120fc4acc13687ef7ed7489ed
Godwin Idemudia <godwinide@gmail.com>
2025-04-16 16:59:54 +0100

    new

--- src/app/admin/celebrities/[id]/edit/page.tsx
+++ src/app/admin/celebrities/[id]/edit/page.tsx
@@ -1,6 +1,6 @@
 import CelebrityComponent from "./CelebrityComponent";
 
-export default async function EditCelebrityPage({ params }: { params: { id: string } }) {
+export default async function EditCelebrityPage({ params }: any) {
     const _params = await params;
     return <CelebrityComponent
         params={_params}
